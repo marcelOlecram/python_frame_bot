ed1f3cad70f67e14e5915dcadfb0ca1c845415f8
olecram <marcelo.acosta1994@gmail.com>
2022-02-26 13:39:36 -0400

    Replaced prints with propper logging

--- python_frame_bot.py
+++ python_frame_bot.py
@@ -1,10 +1,24 @@
-import requests
+import logging
 import json
-import schedule
 import time
 import os
+from datetime import datetime
+import requests
+import schedule
 import functools
 from frame_exceptions import FrameException
+
+logger = logging.getLogger('frame-bot')
+logger.setLevel(logging.INFO)
+start_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.log")
+fileHandler = logging.FileHandler(filename=f"frame_bot-{start_time}", mode='w')
+fileHandler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - [%(name)s][%(levelname)s] - %(message)s')
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
+logger.addHandler(consoleHandler)
 
 # Constants to get configuration data
 CONFIG_FILE = "config.txt"
@@ -15,6 +29,8 @@ TOTAL_FRAMES = "total_frames"
 POST_EVERY_SEGS = "post_every_segs"
 EPISODE_NAME = "episode_name"
 SHOW_NAME = "show_name"
+START_FRAME = "start_frame"
+FRAME_HOST = "frame_host"
 
 # Global variables. Will be loaded from config.txt
 page_id = ''
@@ -25,6 +41,8 @@ post_every_segs = 0
 show_name=''
 episode_name = ''
 frames_posted = 0
+start_frame = 0
+frame_host = ''
 
 #Handling exceptions (based on schedule module docs)
 def catch_exceptions(cancel_on_failure=False):
@@ -35,13 +53,13 @@ def catch_exceptions(cancel_on_failure=False):
                 return job_func(*args, **kwargs)
             except FrameException as fe:
                 post_facebook_message(fe.get_message)
-                print(fe.message)
+                logger.error(fe.message)
                 if cancel_on_failure:
                     return schedule.CancelJob            
             except:
                 import traceback
                 post_facebook_message(traceback.format_exc())
-                print(traceback.format_exc())
+                logger.error(traceback.format_exc())
                 if cancel_on_failure:
                     return schedule.CancelJob            
 
@@ -52,55 +70,64 @@ def load_config():
     """
     Loads the configuration data from config.txt
     """
-    print("Loading configs")
+    logger.info("Loading configs")
     if(not os.path.isfile(CONFIG_FILE)):
-        print("No config file found")
+        logger.error("No config file found")
         return False        
     file_config = open("config.txt","r")
     for file_line in file_config:
         if ACCESS_TOKEN in file_line :
             global access_token
             access_token = file_line.split(':')[1].strip()
-            print("access_token found")
+            logger.debug("access_token found")
             continue
         if PAGE_ID in file_line:
             global page_id
             page_id = file_line.split(':')[1].strip()
-            print("page_id found")
+            logger.debug("page_id found")
             continue
         if ALBUM_ID in file_line:
             global album_id
             album_id = file_line.split(':')[1].strip()
-            print("album_id found")
+            logger.debug("album_id found")
             continue
         if TOTAL_FRAMES in file_line:
             global total_frames
             total_frames = int(file_line.split(':')[1].strip())
-            print("total_frames found")
+            logger.debug("total_frames found")
             continue
         if POST_EVERY_SEGS in file_line:
             global post_every_segs
             post_every_segs = int(file_line.split(':')[1].strip())
-            print("post_every_segs found")
+            logger.debug("post_every_segs found")
             continue    
         if EPISODE_NAME in file_line:
             global episode_name
             episode_name = file_line.split(':')[1].strip()
-            print("episode_name found")
+            logger.debug("episode_name found")
             continue    
         if SHOW_NAME in file_line:
             global show_name
             show_name = file_line.split(':')[1].strip()
-            print("show_name found")
+            logger.debug("show_name found")
             continue    
+        if START_FRAME in file_line:
+            global start_frame
+            start_frame = int(file_line.split(':')[1].strip())
+            logger.debug("start_frame found")
+            continue    
+        if FRAME_HOST in file_line:
+            global frame_host
+            frame_host = file_line.split(':')[1].strip()
+            logger.debug("frame_host found")
+            continue 
+    logger.info("Config loaded")
     return True
 
 def post_facebook_message(message:str):
     """
     Post a single text to page
 
-    Parameters
-    --------
     message: str
         Text to post
     """
@@ -119,10 +146,10 @@ def post_album_frame_facebook():
 
     Post the image stored, the url of the image is calculated
     """
-    print(f"Frame: {frames_posted} of {total_frames}")
+    logger.info(f"Frame: {frames_posted} of {total_frames}")
     facebook_host_api = 'https://graph.facebook.com/{}/photos'.format(album_id)
     frame_file_name = get_frame_file_name(frames_posted)
-    image_location = 'https://frame-bot.s3.amazonaws.com/etrotshfio-framebot/{0}/{1}'.format(episode_name, frame_file_name)
+    image_location = '{0}/{1}/{2}'.format(frame_host, episode_name, frame_file_name)
     img_payload = {
     'url': image_location,
     'access_token': access_token,
@@ -142,15 +169,16 @@ def get_frame_file_name(frame):
     return "Shield-Hero-{0}-f{1}.jpg".format(episode_name,frame_zfill)
 
 def main():
-    print("Frame bot started.")
+
+    logger.info("Frame bot started.")
     if not load_config():
-        print("No config file found")
         return
 
     global frames_posted
+    frames_posted = start_frame
     schedule.every(post_every_segs).seconds.do(post_album_frame_facebook)
     condition = frames_posted < total_frames
-    print("Started posting frames")
+    logger.info("Started posting frames")
     ellapsed_seconds = 0
     while condition:
         schedule.run_pending()
@@ -159,7 +187,7 @@ def main():
             condition = frames_posted <= total_frames
         time.sleep(1)
         ellapsed_seconds += 1
-    print("All frames posted")
+    logger.info("All frames posted")
 
 if __name__ == '__main__':
     main()    
